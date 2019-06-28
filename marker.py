bd12b4a21ac33c58b875315df09fd2ca629642bb
2017029525 <yungkk87@hanyang.ac.kr>
2019-06-28 16:15:49 +0900

    change marker module (apriltag->aruco)

--- marker.py
+++ marker.py
@@ -22,5 +22,23 @@ def make_marker():
     plt.show()
 
 
+def detect_marker(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    parameters = aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+
+    plt.figure()
+    plt.imshow(frame_markers)
+    for i in range(len(ids)):
+        c = corners[i][0]
+        plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "o", label="id={0}".format(ids[i]))
+    plt.legend()
+    plt.show()
+
+
 if __name__=='__main__':
-    make_marker()
+    # make_marker()
+    frame = cv2.imread('./data/test_image/arucotag.jpg')
+    detect_marker(frame)
