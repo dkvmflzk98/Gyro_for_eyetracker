1e49c0737443c81881bb7ba48f2d0fe34acf2cc3
dkvmflzk98 <yungkk87@gmail.com>
2019-07-05 20:42:43 +0900

    test dot operate

--- rotate.py
+++ rotate.py
@@ -1,8 +1,13 @@
 from scipy.spatial.transform import Rotation as R
+import numpy as np
 
 if __name__=='__main__':
     with open('./gyro_record_20190704-205009.csv') as f:
         lines = f.readlines()
+        pos = np.arange(3)
+        print(pos)
         for line in lines[2:10]:
             r = R.from_quat(line.split(',')[1:5])
             print(r.as_dcm())
+
+            print(r.as_dcm().dot(pos))
