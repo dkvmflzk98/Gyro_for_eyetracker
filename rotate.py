b7d4e707328808805abc56f602c3f8f7be5a1082
HyeongJin Kim <yungkk87@gmail.com>
2019-07-23 20:06:03 +0900

    [feat] use gyro data for heatmap
    rotate.py-add code for draw heatmap
    visualize.py-create function for external use

--- rotate.py
+++ rotate.py
@@ -1,5 +1,7 @@
 from scipy.spatial.transform import Rotation
 import numpy as np
+import pandas as pd
+from visualize import sphere_heatmap
 
 
 def as_spherical(cartesian):
@@ -11,16 +13,35 @@ def as_spherical(cartesian):
 
 
 if __name__ == '__main__':
-    with open('./gyro_record_20190704-205009.csv') as f:
+    write_file = False
+    visualize = True
+    filename = './gyro_record_20190704-205009.csv'
+
+    with open(filename) as f:
         lines = f.readlines()
         pos = np.array([1, 0, 0])
-        print(pos)
-        with open('./gyro_spherical_record_20190704-205009.csv', 'w') as rf:
-            # theta: 0~180, phi: -180~180
-            rf.write('n,r,theta,phi\n')
-            for n, line in enumerate(lines[1:]):
-                out = str(n)
-                rot_info = Rotation.from_quat(line.split(',')[1:5])
-                for value in as_spherical(rot_info.as_dcm().dot(pos)):
-                    out += ',' + str(value)
-                rf.write(out + '\n')
+
+        if write_file:
+            with open('./gyro_spherical_record' + filename.split('_')[2], 'w') as rf:
+                # theta: 0~180, phi: -180~180
+                rf.write('n,r,theta,phi\n')
+                for n, line in enumerate(lines[1:]):
+                    out = str(n)
+                    rot_info = Rotation.from_quat(line.split(',')[1:5])
+                    result_vector = rot_info.as_dcm().dot(pos)
+
+                    for value in as_spherical(result_vector):
+                        out += ',' + str(value)
+                    rf.write(out + '\n')
+
+        if visualize:
+            df = pd.read_csv(filename, sep=',', header=0)
+            rot_data = Rotation.from_quat(df.values[:, 1:5])
+            gaze_vec = np.array([[1, 0, 0] for _ in range(df.shape[0])])
+            rot_gaze = np.array([as_spherical(rot_info.as_dcm().dot(gaze)) for rot_info, gaze in zip(rot_data, gaze_vec)])
+            rot_gaze_norm = np.array(rot_gaze)
+
+            theta, phi = np.linspace(0, 180, 74, endpoint=True), np.linspace(-180, 180, 145, endpoint=False)
+            gaze_map, _, _ = np.histogram2d(rot_gaze[:, 1], rot_gaze[:, 2], [theta, phi])
+
+            sphere_heatmap(np.linspace(-180, 180, 144, endpoint=False), np.linspace(-90, 90, 73, endpoint=True), gaze_map)
--- visualize.py
+++ visualize.py
@@ -1,9 +1,7 @@
 import plotly.plotly as py
 import numpy as np
 from numpy import pi, sin, cos
-from scipy.io import netcdf
 from mpl_toolkits.basemap import Basemap
-import warnings
 
 
 def degree2radians(degree):
@@ -22,96 +20,90 @@ def mapping_map_to_sphere(lon, lat, radius=1):
     return xs, ys, zs
 
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    with netcdf.netcdf_file('compday.x2xxBFLhKB.nc', 'r') as f:
-        print(f.variables)
-        lon = f.variables['lon'][::]
-        lat = f.variables['lat'][::-1]
-        olr = f.variables['air'][0, ::-1, :]
+def sphere_heatmap(lon, lat, olr):
+    print(lon.shape, lat.shape, olr.shape)
 
-tmp_lon = np.array([lon[n]-360 if l >= 180 else lon[n] for n, l in enumerate(lon)])
+    tmp_lon = np.array([lon[n]-360 if l >= 180 else lon[n] for n, l in enumerate(lon)])
 
-i_east, = np.where(tmp_lon >= 0)
-i_west, = np.where(tmp_lon < 0)
-lon = np.hstack((tmp_lon[i_west], tmp_lon[i_east]))
+    i_east, = np.where(tmp_lon >= 0)
+    i_west, = np.where(tmp_lon < 0)
+    lon = np.hstack((tmp_lon[i_west], tmp_lon[i_east]))
 
-olr_ground = np.array(olr)
-olr = np.hstack((olr_ground[:, i_west], olr_ground[:, i_east]))
-print(olr)
+    olr_ground = np.array(olr)
+    olr = np.hstack((olr_ground[:, i_west], olr_ground[:, i_east]))
 
-# Make shortcut to Basemap object,
-# not specifying projection type for this example
-m = Basemap()
+    # Make shortcut to Basemap object,
+    # not specifying projection type for this example
+    m = Basemap()
 
-colorscale = [[0.0, '#313695'],
- [0.07692307692307693, '#3a67af'],
- [0.15384615384615385, '#5994c5'],
- [0.23076923076923078, '#84bbd8'],
- [0.3076923076923077, '#afdbea'],
- [0.38461538461538464, '#d8eff5'],
- [0.46153846153846156, '#d6ffe1'],
- [0.5384615384615384, '#fef4ac'],
- [0.6153846153846154, '#fed987'],
- [0.6923076923076923, '#fdb264'],
- [0.7692307692307693, '#f78249'],
- [0.8461538461538461, '#e75435'],
- [0.9230769230769231, '#cc2727'],
- [1.0, '#a50026']]
+    colorscale = [[0.0, '#313695'],
+     [0.07692307692307693, '#3a67af'],
+     [0.15384615384615385, '#5994c5'],
+     [0.23076923076923078, '#84bbd8'],
+     [0.3076923076923077, '#afdbea'],
+     [0.38461538461538464, '#d8eff5'],
+     [0.46153846153846156, '#d6ffe1'],
+     [0.5384615384615384, '#fef4ac'],
+     [0.6153846153846154, '#fed987'],
+     [0.6923076923076923, '#fdb264'],
+     [0.7692307692307693, '#f78249'],
+     [0.8461538461538461, '#e75435'],
+     [0.9230769230769231, '#cc2727'],
+     [1.0, '#a50026']]
 
-clons = np.array(lon.tolist()+[180], dtype=np.float64)
-clats = np.array(lat, dtype=np.float64)
-clons, clats = np.meshgrid(clons, clats)
-XS, YS, ZS = mapping_map_to_sphere(clons, clats)
+    clons = np.array(lon.tolist()+[180], dtype=np.float64)
+    clats = np.array(lat, dtype=np.float64)
+    clons, clats = np.meshgrid(clons, clats)
+    XS, YS, ZS = mapping_map_to_sphere(clons, clats)
 
-nrows, ncolumns = clons.shape
-OLR = np.zeros(clons.shape, dtype=np.float64)
-OLR[:, :ncolumns-1] = np.copy(np.array(olr,  dtype=np.float64))
-OLR[:, ncolumns-1] = np.copy(olr[:, 0])
-print(OLR)
+    nrows, ncolumns = clons.shape
+    OLR = np.zeros(clons.shape, dtype=np.float64)
+    OLR[:, :ncolumns-1] = np.copy(np.array(olr,  dtype=np.float64))
+    OLR[:, ncolumns-1] = np.copy(olr[:, 0])
+    print(OLR)
 
-text = [['lon: '+'{:.2f}'.format(clons[i, j])+'<br>lat: '+'{:.2f}'.format(clats[i, j]) +
-        '<br>W: '+'{:.2f}'.format(OLR[i][j]) for j in range(ncolumns)] for i in range(nrows)]
+    text = [['lon: '+'{:.2f}'.format(clons[i, j])+'<br>lat: '+'{:.2f}'.format(clats[i, j]) +
+            '<br>W: '+'{:.2f}'.format(OLR[i][j]) for j in range(ncolumns)] for i in range(nrows)]
 
-sphere = dict(type='surface',
-            x=XS,
-            y=YS,
-            z=ZS,
-            colorscale=colorscale,
-            surfacecolor=OLR,
-            cmin=np.min(OLR),
-            cmax=np.max(OLR),
-            colorbar=dict(thickness=20, len=0.75, ticklen=4, title= 'W/m²'),
-            text=text,
-            hoverinfo='text')
+    sphere = dict(type='surface',
+                x=XS,
+                y=YS,
+                z=ZS,
+                colorscale=colorscale,
+                surfacecolor=OLR,
+                cmin=np.min(OLR),
+                cmax=np.max(OLR),
+                colorbar=dict(thickness=20, len=0.75, ticklen=4, title= 'W/m²'),
+                text=text,
+                hoverinfo='text')
 
-noaxis = dict(showbackground=False,
-              showgrid=False,
-              showline=False,
-              showticklabels=False,
-              ticks='',
-              title='',
-              zeroline=False)
+    noaxis = dict(showbackground=False,
+                  showgrid=False,
+                  showline=False,
+                  showticklabels=False,
+                  ticks='',
+                  title='',
+                  zeroline=False)
 
-layout3d = dict(title='Outgoing Longwave Radiation Anomalies<br>Dec 2017-Jan 2018',
-                font=dict(family='Balto', size=14),
-                width=800,
-                height=800,
-                scene=dict(xaxis=noaxis,
-                           yaxis=noaxis,
-                           zaxis=noaxis,
-                           aspectratio=dict(x=1,
-                                            y=1,
-                                            z=1),
-                           camera=dict(eye=dict(x=1.15,
-                                                y=1.15,
-                                                z=1.15)
-                                       )
-                           ),
-                paper_bgcolor='rgba(235,235,235, 0.9)'
-                )
+    layout3d = dict(title='Outgoing Longwave Radiation Anomalies<br>Dec 2017-Jan 2018',
+                    font=dict(family='Balto', size=14),
+                    width=800,
+                    height=800,
+                    scene=dict(xaxis=noaxis,
+                               yaxis=noaxis,
+                               zaxis=noaxis,
+                               aspectratio=dict(x=1,
+                                                y=1,
+                                                z=1),
+                               camera=dict(eye=dict(x=1.15,
+                                                    y=1.15,
+                                                    z=1.15)
+                                           )
+                               ),
+                    paper_bgcolor='rgba(235,235,235, 0.9)'
+                    )
 
-fig = dict(data=[sphere], layout=layout3d)
-py.sign_in('yungkk87', 'hFD4gMKjr5YyORuEWCOz')
-py.plot(fig, filename='radiation-map2sphere')
+    fig = dict(data=[sphere], layout=layout3d)
+    py.sign_in('yungkk87', 'hFD4gMKjr5YyORuEWCOz')
+    py.plot(fig, filename='radiation-map2sphere')
 
